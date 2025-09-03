da5313a9a8280823ef3c16f92240c1226b728ee1
Matias <matiasprofe2021@gmail.com>
2025-09-02 21:43:05 -0300

    Ejercicio 12

--- Ejercicio_!/Alumno.cs
+++ Ejercicio_!/Alumno.cs
@@ -4,15 +4,21 @@ using System;
 
 namespace Ejercicio__
 {
-	/// <summary>
-	/// Description of Alumno.
-	/// </summary>
-	public class Alumno
+	public class Alumno : Persona
 	{
 		private double legajo;
+		private double promedio;
 		
-		public Alumno()
+		
+		public Alumno(string n, double d, double l, double p) : base(n,d)
 		{
+			this.legajo = l;
+			this.promedio = p;
 		}
+		
+		public double Legajo{get;set;}
+		public double Promedio{get;set;}
+		
+		
 	}
 }
