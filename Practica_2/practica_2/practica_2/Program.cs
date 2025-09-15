cca5343af376fae4fd14a516e0112a65777657d4
Matias <matiasprofe2021@gmail.com>
2025-09-15 15:51:33 -0300

    Agrego carpeta nueva

---
+++ Practica_2/practica_2/practica_2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mati1
+ * Date: 15/9/2025
+ * Time: 15:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace practica_2
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
