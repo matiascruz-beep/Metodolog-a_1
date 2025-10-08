fb46e1cd18d2a1a64a76d7bb87db389bbe0f96d7
Matias <matiasprofe2021@gmail.com>
2025-10-08 07:22:09 -0300

    Agrego ractica 4

---
+++ Practica_2/practica3/FabricaDeProfesores.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mati1
+ * Date: 29/9/2025
+ * Time: 16:58
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace practica3
+{
+	/// <summary>
+	/// Description of FabricaDeProfesores.
+	/// </summary>
+	public class FabricaDeProfesores
+	{
+		public FabricaDeProfesores()
+		{
+		}
+	}
+}
---
+++ Practica_2/practica3/Observador.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mati1
+ * Date: 30/9/2025
+ * Time: 21:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace practica3
+{
+	/// <summary>
+	/// Description of Observador.
+	/// </summary>
+	public class Observador
+	{
+		public Observador()
+		{
+		}
+	}
+}
